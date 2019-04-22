7eee97d4a2652b68af3dae433cec7087ae69624e
Iskandar Askarov <iskandar.askarov@gmail.com>
2019-04-21 23:49:46 -0400

    initial commit

---
+++ src/boburchik.cpp
@@ -0,0 +1,39 @@
+//============================================================================
+// Name        : boburchik.cpp
+// Author      : Iskandar Askarov
+// Version     :
+// Copyright   : Use it all you want!!!
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+clock_t timekeeper;
+
+struct _input{
+
+};
+
+
+int main(int argc, char **argv) {
+
+	if(argc != 3){
+		cout << "Usage: ./huffman8515 INPUT_FILE OUTPUT_FILE" << endl;
+		return EXIT_FAILURE;
+	}
+
+	timekeeper = clock_t();
+
+
+
+
+	return EXIT_SUCCESS;
+}
+
+
+static void init_file(char *pinput){
+
+}
